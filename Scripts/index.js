ccd9c24e0cf27fc4ac635170fb49929ffa098a69
Yerin99 <63496777+Yerin99@users.noreply.github.com>
2022-01-13 20:55:36 +0900

    Feat : show list & add, delete, complete items

---
+++ Scripts/index.js
@@ -0,0 +1,48 @@
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const output = document.querySelector("ul");
+
+function outputList(text) {
+    var item = createItem(text);
+    output.appendChild(item);
+}
+
+function createItem(text) {
+    const item = document.createElement("li");
+    const toDo = document.createElement("span");
+    const completeBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
+    toDo.innerText = text;
+    completeBtn.innerText = "complete";
+    completeBtn.addEventListener("click", completeItem);
+    deleteBtn.innerText = "delete";
+    deleteBtn.addEventListener("click", deleteItem);
+    item.appendChild(toDo);
+    item.appendChild(completeBtn);
+    item.appendChild(deleteBtn);
+    return item;
+}
+
+function deleteItem() {
+    this.addEventListener("click", output.removeChild(this.parentNode));
+}
+
+function completeItem() {
+    const toDo = this.parentNode.querySelector("span");
+    toDo.style.textDecoration = "line-through";
+}
+
+function submitHandler(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    if (currentValue != "") {
+        outputList(currentValue);
+    }
+    input.value = "";
+}
+
+function init() {
+    form.addEventListener("submit", submitHandler);
+}
+
+init();
